cea22dba015811aae1dc798d64158cb14bc7e0de
Arthur De Neyer <arthur.deneyer@student.uclouvain.be>
2023-02-27 17:25:11 +0100

    In Terminal

---
+++ backend/plot.py
@@ -0,0 +1,77 @@
+import plotext as plt
+import os, psutil, time
+from math import *
+import argparse 
+
+
+def cpu():
+    while True:
+        list_cores = psutil.cpu_percent(interval=1, percpu=True)
+        cpu_usage = [ceil(sum(list_cores) / len(list_cores))] + [ceil(i) for i in list_cores]
+
+        cpu_name = ["General:"] + [f"CPU{i+1}:" for i in range(len(cpu_usage))]
+
+        rest = [100 - cpu_usage[i] for i in range(len(cpu_usage))]
+
+        
+        plt.cld()
+
+        plt.simple_stacked_bar(cpu_name, [cpu_usage, rest], width=100, labels=["used", "free"], title="CPU Cores' usage", colors=["red", "white"])
+
+        plt.sleep(0.01)
+        os.system('cls' if os.name == 'nt' else 'clear')
+        plt.show()
+
+def ram():
+    while True:
+        ram = ceil(psutil.virtual_memory()[2])
+        free = 100 - ram
+
+        plt.cld()
+        plt.simple_stacked_bar(["RAM:"], [[ram], [free]], title="RAM usage", labels=["used", "free"], colors=["red", "white"])
+        plt.sleep(0.01)
+        os.system('cls' if os.name == 'nt' else 'clear')
+        plt.show()
+        time.sleep(1)
+
+def battery():
+    def convertTime(seconds):
+        minutes, seconds = divmod(seconds, 60)
+        hours, minutes = divmod(minutes, 60)
+        return "%d:%02d:%02d" % (hours, minutes, seconds)
+    while True:
+        btry = psutil.sensors_battery()
+        btry_percent = ceil(btry.percent)
+        free = 100 - btry_percent
+        plugged = btry.power_plugged
+        time_left = convertTime(btry.secsleft)
+
+        plt.cld()
+        plt.simple_stacked_bar(["Battery:"], [[btry_percent], [free]], title="Battery Percentage", labels=["available", "used"], colors=["green", "white"])
+        plt.sleep(0.01)
+        os.system('cls' if os.name == 'nt' else 'clear')
+        plt.show()
+        print(f"Battery Plugged: {plugged}")
+        print(f"Time left: {time_left if not plugged else 'N/A'}")
+        time.sleep(1)
+
+# def help():
+#     print("List of usabla arguments:\n")
+#     print("--help: get help")
+#     print("-cpu: shows CPU cores' usage")
+#     print("-ram: shows ram usage")
+#     print("-battery (or -btry): shows battery informations")
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Simple System Monitor')
+    parser.add_argument('-cpu', action='store_true', help="Shows CPU cores' usage")
+    parser.add_argument('-ram', action='store_true', help='Shows the RAM usage')
+    parser.add_argument('-btry', action='store_true', help='Shows the battery usage')
+
+    args = parser.parse_args()
+    if args.cpu:
+        cpu()
+    if args.ram:
+        ram()
+    if args.btry:
+        battery()
--- backend/retriever.py
+++ backend/retriever.py
@@ -1,9 +1,65 @@
-import os
 import psutil
- 
-# Getting loadover15 minutes
-load1, load5, load15 = psutil.getloadavg()
- 
-cpu_usage = (load15/os.cpu_count()) * 100
- 
-print("The CPU usage is : ", cpu_usage)
+import time
+from math import *
+
+from colorama import Fore, Style, init
+init(autoreset=True)
+
+
+
+
+# python script showing battery details
+import psutil
+
+# function returning time in hh:mm:ss
+def convertTime(seconds):
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    return "%d:%02d:%02d" % (hours, minutes, seconds)
+
+class Monitor:
+    def get_cpu_usage():
+        
+        return psutil.cpu_percent()
+    
+
+
+
+while True:
+
+    print("==========================")
+    cores_percent = psutil.cpu_percent(percpu=True)
+    general_percent = psutil.cpu_percent(percpu=False)
+
+    charged = ceil(general_percent // 2)
+    free = 50 - charged
+    charged_vis = '-' * charged
+    free_vis = '-' * free
+
+    per = f"{Fore.RED + charged_vis + Style.RESET_ALL}{Fore.GREEN + free_vis + Style.RESET_ALL}"
+    cp_percents = f"General percent: {general_percent} {per}\n\n"
+
+    for i in range(len(cores_percent)):
+        charged = ceil(cores_percent[i] // 2)
+        free = 50 - charged
+        charged_vis = '-' * charged
+        free_vis = '-' * free
+
+        per = f"{Fore.RED + charged_vis + Style.RESET_ALL}{Fore.GREEN + free_vis + Style.RESET_ALL}"
+
+        cp_percents += f"CPU{i+1}: {cores_percent[i]}% {per}\n"
+
+    print(cp_percents)
+
+    print("The ram usage is: %", psutil.virtual_memory()[2])
+
+    # returns a tuple
+    battery = psutil.sensors_battery()
+
+    print("Battery percentage : ", battery.percent)
+    print("Power plugged in : ", battery.power_plugged)
+
+    # converting seconds to hh:mm:ss
+    print("Battery left : ", convertTime(battery.secsleft))
+    print("==========================\n\n")
+    time.sleep(5)
